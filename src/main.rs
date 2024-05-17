c590b8c984b78b4e2daaeb5d630f61a729848b1a
nik <niklas.harnish@hotmail.com>
2024-05-17 20:56:59 +0200

    added noshp integration

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,58 @@ mod audio_player;
 mod ui;
 
 use std::{
-    env::args, fmt::Debug, fs, io::{self, BufRead}, time::Duration, usize
+    env::args,
+    error::Error,
+    fmt::Debug,
+    fs,
+    io::{self, BufRead},
+    time::Duration,
+    usize,
 };
 
 use clap::Parser;
 use soloud::{audio, AudioExt, Handle, LoadExt, Soloud, Wav};
 use NOSHP_Client::{
-    client::{ClientState, NoshpClient, Request, UserDefinedState},
+    client::{self, ClientState, NoshpClient, Request, UserDefinedState},
     client_config::{ClientConfig, ParsedConfig},
 };
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Start Program with UI, default: false 
+    /// Start Program with UI, default: false
     #[arg(short, long, default_value_t = false)]
     ui: bool,
 }
 
-fn main() {
+struct MusicPlayerState {
+    pub sl: Soloud,
+    pub wav: Wav,
+    pub song_list: Vec<String>,
+    pub current_song_index: usize,
+    pub channel_handle: soloud::Handle,
+    pub pause_state: bool,
+    pub volume: f32,
+}
+impl Default for MusicPlayerState {
+    fn default() -> Self {
+        Self {
+            sl: Soloud::default().unwrap(),
+            wav: audio::Wav::default(),
+            song_list: Vec::new(),
+            current_song_index: 0,
+            channel_handle: soloud::Handle::PRIMARY,
+            pause_state: false,
+            volume: 1.0,
+        }
+    }
+}
+impl UserDefinedState for MusicPlayerState {}
+
+const CONFIG_PATH: &str = "./example_config.toml";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let song_list: Vec<String> = fs::read_dir("audio_files")
@@ -35,7 +68,7 @@ fn main() {
     let mut volume = 100.0;
 
     let mut current_song_index = 0;
-    let song_handle =
+    let channel_handle =
         audio_player::next_song(&mut sl, &mut wav, &song_list, &mut current_song_index);
     audio_player::toggle_pause_song(&mut sl, pause_state);
 
@@ -45,12 +78,65 @@ fn main() {
             &mut wav,
             &song_list,
             &mut current_song_index,
-            song_handle,
+            channel_handle,
             &mut pause_state,
             &mut volume,
         );
+    } else {
+        let config = ClientConfig::load_config(CONFIG_PATH).unwrap();
+
+        let state = MusicPlayerState {
+            sl,
+            wav,
+            song_list,
+            current_song_index,
+            channel_handle,
+            pause_state,
+            volume,
+        };
+
+        let client_handler = NoshpClient::new();
+        client_handler
+            .set_state(state)
+            .add_callback("Pause/Play", Box::new(callback_toggle_pause))
+            .add_callback("Next", Box::new(callback_next))
+            .add_callback("Previous", Box::new(callback_prev))
+            .add_callback("Volume", Box::new(callback_vol))
+            .run(config)
+            .await
+            .unwrap();
     }
-    else {
-        
-    }
+    Ok(())
+}
+
+fn callback_toggle_pause(state: &mut ClientState<MusicPlayerState>, _req: Request) {
+    state.user_state.pause_state =
+        audio_player::toggle_pause_song(&mut state.user_state.sl, state.user_state.pause_state);
+}
+
+fn callback_next(state: &mut ClientState<MusicPlayerState>, _req: Request) {
+    state.user_state.channel_handle = audio_player::next_song(
+        &mut state.user_state.sl,
+        &mut state.user_state.wav,
+        &state.user_state.song_list,
+        &mut state.user_state.current_song_index,
+    );
+}
+
+fn callback_prev(state: &mut ClientState<MusicPlayerState>, _req: Request) {
+    state.user_state.channel_handle = audio_player::prev_song(
+        &mut state.user_state.sl,
+        &mut state.user_state.wav,
+        &state.user_state.song_list,
+        &mut state.user_state.current_song_index,
+    );
+}
+
+fn callback_vol(state: &mut ClientState<MusicPlayerState>, req: Request) {
+    state.user_state.volume = req.value.unwrap();
+    audio_player::set_volume(
+        &mut state.user_state.sl,
+        state.user_state.channel_handle,
+        state.user_state.volume,
+    )
 }
